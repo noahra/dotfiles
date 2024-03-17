8a46399e74f2666f209fe540926a01bbc24d195e
Noah Rahimzadagan <nora@netlight.com>
2024-03-17 23:56:36 +0100

    bug fix

--- lua/noah/lazy.lua
+++ lua/noah/lazy.lua
@@ -11,16 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-
-  {import = "noah.plugins"},
-  {import = "noah.plugins.lsp"},
-})
-
-
 require("lazy").setup({ { import = "noah.plugins" }, { import = "noah.plugins.lsp" } }, {
   install = {
-    colorscheme = { "kanagawa-dragon" },
+    colorscheme = { "tokyonight-storm" },
   },
   checker = {
     enabled = true,
